d27c883cff0c46648c83eb5191d79f024dffe8fa
Rex-YHA <rex@techstudio.com.sg>
2026-02-05 16:01:00 +0800

    Use node ts-node loader for worker

--- src/scripts/importWorker.ts
+++ src/scripts/importWorker.ts
@@ -1,10 +1,10 @@
 import os from "node:os";
 import path from "node:path";
 import fs from "node:fs/promises";
-import { prisma } from "../lib/prisma.js";
-import { parseFeAnswerPdf } from "../../lib/importer/feAnswerParser.js";
-import { parseFeQuestionPdf } from "../../lib/importer/feQuestionParser.js";
-import { renderPdfPagesToPng } from "../lib/importer/pdfPageRenderer.js";
+import { prisma } from "../lib/prisma";
+import { parseFeAnswerPdf } from "../../lib/importer/feAnswerParser";
+import { parseFeQuestionPdf } from "../../lib/importer/feQuestionParser";
+import { renderPdfPagesToPng } from "../lib/importer/pdfPageRenderer";
 import "dotenv/config";
 
 const LOCK_TIMEOUT_MINUTES = 10;
